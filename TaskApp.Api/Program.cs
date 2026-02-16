f09cbde83cde54731288277d49a6d0dff95be3f7
Ahmed Herhesh <ahmedherhesh3@gmail.com>
2026-02-16 22:12:11 +0200

    JWT configration

--- TaskApp.Api/Program.cs
+++ TaskApp.Api/Program.cs
@@ -1,12 +1,43 @@
+using System.Text;
 using FluentValidation;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using TaskApp.Api.Middlewares;
 using TaskApp.Application;
 using TaskApp.Application.Behaviors;
+using TaskApp.Infrastructure.Authentication;
 using TaskApp.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+var jwtConfig = builder.Configuration.GetSection("Jwt").Get<Jwt>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtConfig!.Issuer,
+        ValidAudience = jwtConfig.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtConfig.SecretKey))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
