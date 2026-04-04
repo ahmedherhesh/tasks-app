ab436c3f6928d86b18644dbd19efd3e55e7533fe
ahmedherhesh <ahmedherhesh3@gmail.com>
2026-04-04 21:25:41 +0200

    CreatedById To All Tables

---
+++ TaskApp.Infrastructure/Migrations/20260404192423_CreatedById.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedById : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_AspNetUsers_CreatedById",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_AspNetUsers_CreatedById",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskTimes_AspNetUsers_CreatedById",
+                table: "TaskTimes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskTimes_CreatedById",
+                table: "TaskTimes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_CreatedById",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_CreatedById",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CreatedById",
+                table: "TaskTimes",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById1",
+                table: "TaskTimes",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CreatedById",
+                table: "Tasks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById1",
+                table: "Tasks",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CreatedById",
+                table: "Projects",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById1",
+                table: "Projects",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskTimes_CreatedById1",
+                table: "TaskTimes",
+                column: "CreatedById1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CreatedById1",
+                table: "Tasks",
+                column: "CreatedById1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_CreatedById1",
+                table: "Projects",
+                column: "CreatedById1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_AspNetUsers_CreatedById1",
+                table: "Projects",
+                column: "CreatedById1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_AspNetUsers_CreatedById1",
+                table: "Tasks",
+                column: "CreatedById1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskTimes_AspNetUsers_CreatedById1",
+                table: "TaskTimes",
+                column: "CreatedById1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_AspNetUsers_CreatedById1",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_AspNetUsers_CreatedById1",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskTimes_AspNetUsers_CreatedById1",
+                table: "TaskTimes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskTimes_CreatedById1",
+                table: "TaskTimes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_CreatedById1",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_CreatedById1",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById1",
+                table: "TaskTimes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById1",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById1",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "TaskTimes",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "Tasks",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedById",
+                table: "Projects",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskTimes_CreatedById",
+                table: "TaskTimes",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CreatedById",
+                table: "Tasks",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_CreatedById",
+                table: "Projects",
+                column: "CreatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_AspNetUsers_CreatedById",
+                table: "Projects",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_AspNetUsers_CreatedById",
+                table: "Tasks",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskTimes_AspNetUsers_CreatedById",
+                table: "TaskTimes",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
